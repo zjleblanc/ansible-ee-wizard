26e5cf011aca6b3feb57c9b58e78f5f0ed6b127e
zjleblanc <zjleblanc3@gmail.com>
2023-03-02 11:03:55 -0600

    add examples

---
+++ src/app/constants/bindep-examples.ts
@@ -0,0 +1,12 @@
+export const BINDEP_EXAMPLES = [
+  "krb5-devel [platform:redhat]",
+  "libffi-dev [platform:rpm]",
+  "libffi-devel [platform:redhat]",
+  "libjpeg-dev [platform:dpkg]",
+  "mariadb-devel [platform:redhat]",
+  "nss-devel [platform:redhat]",
+  "libssl-dev [platform:dpkg]",
+  "openldap-devel [platform:redhat]",
+  "pkgconfig [platform:redhat]",
+  "postgresql-devel [platform:redhat]"
+];
