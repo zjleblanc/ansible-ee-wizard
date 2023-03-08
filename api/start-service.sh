d422179c93e99e3949bb84f96d9ae8e9e680ef91
zjleblanc <zjleblanc3@gmail.com>
2023-03-07 22:26:55 -0600

    specify port

--- api/start-service.sh
+++ api/start-service.sh
@@ -3,6 +3,6 @@ REPO_DIR="$HOME/ansible-ee-wizard"
 SSL_CERT="/etc/letsencrypt/flask.autodotes.com/fullchain.pem"
 SSL_KEY="/etc/letsencrypt/flask.autodotes.com/privkey.pem"
 
-$REPO_DIR/.venv/bin/gunicorn -b 0.0.0.0 \
+$REPO_DIR/.venv/bin/gunicorn -b 0.0.0.0:5000 \
   --certfile=$SSL_CERT --keyfile=$SSL_KEY \
   --chdir $REPO_DIR api:app
